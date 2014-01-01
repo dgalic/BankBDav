a1880bca19eaab39835398fc7c8d72eb08f089d9
Galichet David <galichet.david@gmail.com>
2014-01-01 20:08:22 +0100

    ajout des fonction test si une banque ou une personne existe

--- src/fonction.sql
+++ src/fonction.sql
@@ -1,4 +1,4 @@
--- test fonction d'identification du client et du compte et de la banque
+-- list les personnes existante
 CREATE OR REPLACE FUNCTION list_personne()
 RETURNS TABLE(nom varchar(20), prenom varchar(20)) as $$
 DECLARE
@@ -10,6 +10,58 @@ BEGIN
     RETURN QUERY
     SELECT client.n, client.p;
     END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+---------------------
+
+-- list les banques existante
+CREATE OR REPLACE FUNCTION list_banque()
+RETURNS TABLE(nom varchar(20)) as $$
+DECLARE
+    banque record;
+BEGIN
+    FOR banque IN
+    SELECT nom_banque as b FROM banque
+    LOOP
+    RETURN QUERY
+    SELECT client.b;
+    END LOOP;
+END;
+$$ LANGUAGE plpgsql;
+---------------------
+
+-- test si une personne existe
+CREATE OR REPLACE FUNCTION is_personne(nom text, prenom text)
+RETURNS boolean as $$
+DECLARE
+    client record;
+BEGIN
+    FOR client IN
+    SELECT nom_personne as n, prenom_personne as p FROM personne
+    LOOP
+    IF client.n = nom and client.p = prenom THEN
+        RETURN true;
+    END IF;
+    END LOOP;
+    RETURN false;
+END;
+$$ LANGUAGE plpgsql;
+---------------------
+
+-- test si une banque existe
+CREATE OR REPLACE FUNCTION is_banque(nom text)
+RETURNS boolean as $$
+DECLARE
+    banque record;
+BEGIN
+    FOR banque IN
+    SELECT nom_banque as b FROM banque
+    LOOP
+    IF client.n = nom THEN
+        RETURN true;
+    END IF;
+    END LOOP;
+    RETURN false;
 END;
 $$ LANGUAGE plpgsql;
 ---------------------
