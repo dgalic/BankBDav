6d0c4482891ae277a218704819bcd580e726db95
Galichet David <galichet.david@gmail.com>
2014-01-01 19:50:42 +0100

    ajout de donnee.sql

---
+++ src/donnee.sql
@@ -0,0 +1,15 @@
+-- Insertion des données
+
+INSERT INTO temps (jour) VALUES
+(1);
+
+INSERT INTO personne (nom_personne, prenom_personne) VALUES
+('Galichet','David'),
+('Cruise','TOM'),
+('Pitt','Brad');
+
+INSERT INTO banque (nom_banque) VALUES
+('Bnp'),
+('Lcl'),
+('Cic'),
+('Axa');
