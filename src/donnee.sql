131edb05b7c1af98496a11698c7b6acfc0a32390
david <galichet.david@gmail.com>
2014-01-03 18:45:05 +0100

    dans donnee.sql utilise la fonction creation_banque

--- src/donnee.sql
+++ src/donnee.sql
@@ -8,11 +8,10 @@ INSERT INTO personne (nom_personne, prenom_personne) VALUES
 ('Cruise','TOM'),
 ('Pitt','Brad');
 
-INSERT INTO banque (nom_banque) VALUES
-('Bnp'),
-('Lcl'),
-('Cic'),
-('Axa');
+select * From creation_banque('Bnp',0,0,0,0,0,0);
+select * From creation_banque('Lcl',0,0,0,0,0,0);
+select * From creation_banque('Cic',0,0,0,0,0,0);
+select * From creation_banque('Axa',0,0,0,0,0,0);
 
 INSERT INTO interdit_bancaire(id_banque, id_client, date_debut) VALUES
 (4,2,3);
