ed0e6cd6a93eabed832b723f4c25b24e6ff5136b
david <galichet.david@gmail.com>
2014-01-04 12:07:53 +0100

    ajout de creation de compte dans les données

--- src/donnee.sql
+++ src/donnee.sql
@@ -8,10 +8,20 @@ INSERT INTO personne (nom_personne, prenom_personne) VALUES
 ('Cruise','TOM'),
 ('Pitt','Brad');
 
-select * From creation_banque('Bnp',0,0,0,0,0,0);
-select * From creation_banque('Lcl',0,0,0,0,0,0);
-select * From creation_banque('Cic',0,0,0,0,0,0);
-select * From creation_banque('Axa',0,0,0,0,0,0);
+select creation_banque('Bnp',0,0,0,0,0,0);
+select creation_banque('Lcl',0,0,0,0,0,0);
+select creation_banque('Cic',0,0,0,0,0,0);
+select creation_banque('Axa',0,0,0,0,0,0);
+
+select ouverture_compte('Galichet','David','Bnp');;
+select ouverture_compte('Galichet','David','Bnp');;
+select ouverture_compte('Galichet','David','Axa');;
+select ouverture_compte('Cruise','TOM','Lcl');;
+select ouverture_compte('Cruise','TOM','Cic');;
+select ouverture_compte('Cruise','TOM','Bnp');;
+select ouverture_compte('Pitt','Brad','Axa');;
+select ouverture_compte('Pitt','Brad','Axa');;
+select ouverture_compte('Pitt','Brad','Axa');;
 
 INSERT INTO interdit_bancaire(id_banque, id_client, date_debut) VALUES
 (4,2,3);
