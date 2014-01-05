8704f00ddc10fd327d86b1a63d4e52df7e793d83
david <galichet.david@gmail.com>
2014-01-05 14:04:03 +0100

    ajout de depot d'argent en espece sur les comptes des client

--- src/donnee.sql
+++ src/donnee.sql
@@ -20,6 +20,16 @@ select ouverture_compte('Pitt','Brad','Axa');;
 select ouverture_compte('Pitt','Brad','Axa');;
 select ouverture_compte('Pitt','Brad','Axa');;
 
+select depot(1,1000,'especes');
+select depot(6,10000,'especes');
+select depot(8,1000,'especes');
+select depot(7,500,'especes');
+select depot(9,6000,'especes');
+select depot(4,1010,'especes');
+select depot(2,200,'especes');
+select depot(5,500,'especes');
+select depot(3,6000,'especes');
+
 INSERT INTO interdit_bancaire(id_banque, id_client, date_debut) VALUES
 (4,2,3);
 
