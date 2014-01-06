c6c677b551dce8a923d4e4fdd4a25a3320a1d029
david <galichet.david@gmail.com>
2014-01-06 10:36:16 +0100

    ajout de tout les fonctions trigger et tables dans drop

--- src/drop.sql
+++ src/drop.sql
@@ -1,3 +1,10 @@
+
+-- Nettoyage des Triggers
+DROP TRIGGER IF EXISTS t_interdit_carte ON carte;
+DROP TRIGGER IF EXISTS t_interdit_paiement ON carte_paiement;
+DROP TRIGGER IF EXISTS t_interdit_credit ON carte_credit;
+DROP TRIGGER IF EXISTS t_interdit_retrait ON carte_retrait;
+
 -- Nettoyage des fonctions du fichier transaction_bancaire.sql
 DROP FUNCTION ouverture_compte
 (nom TEXT, prenom TEXT, n_banque TEXT,
@@ -14,6 +21,9 @@ DROP FUNCTION consultation_historique(id_client_compte INTEGER);
 DROP FUNCTION depot (id_client_compte INTEGER, montant_depot REAL, moyen_paiement type_paiement);
 DROP FUNCTION retrait(id_client_compte INTEGER, montant_retrait REAL, moyen_paiement type_paiement);
 DROP FUNCTION depot_cheque (id_dest INTEGER, montant_depot REAL, id_src INTEGER);
+DROP FUNCTION check_liberations();
+DROP FUNCTION check_decouverts();
+DROP FUNCTION plan_remuneration();
 
 -- Nettoyage des fonctions du fichier fonction.sql
 DROP FUNCTION list_personne();
@@ -42,8 +52,18 @@ DROP FUNCTION to_compte_personne(client_id INTEGER, compte_id INTEGER, banque_id
 DROP FUNCTION from_compte_personne(id INTEGER);
 DROP FUNCTION obtenir_chequier(id_client_compte INTEGER);
 
+-- Nettoyage des fonctions du fichier carte.sql
+DROP FUNCTION id_carte_suivant();
+DROP FUNCTION carte_retrait();
+DROP FUNCTION carte_paiement();
+DROP FUNCTION carte_credit(_id_personne INTEGER, _id_banque INTEGER, _id_compte INTEGER);
+DROP FUNCTION ajoute_revolving(_id_personne INTEGER, _id_carte INTEGER,  montant REAL);
+DROP FUNCTION t_interdit_carte();
+
+-- Nettoyage des fonctions du fichier virement.sql
+DROP FUNCTION virement_unitaire(id_src INTEGER, id_dest INTEGER, mont REAL, cout REAL);
+DROP FUNCTION virement_periodique(id_src INTEGER, id_dest INTEGER, montant REAL, cout_i REAL, inter INTEGER, cout_p REAL, date INTEGER);
 DROP FUNCTION plan_virements();
-DROP FUNCTION plan_remunerations();
 
 -- Nettoyage de la base de donnees
 DROP TABLE plan_virements CASCADE;
@@ -56,9 +76,6 @@ DROP TABLE distributeur CASCADE;
 DROP TABLE personne CASCADE;
 DROP TABLE banque_reference CASCADE;
 DROP TABLE banque CASCADE;
-DROP TABLE carte_retrait CASCADE;
-DROP TABLE carte_paiement CASCADE;
-DROP TABLE carte_credit CASCADE;
 DROP TABLE carte CASCADE;
 DROP TABLE virement CASCADE;
 
